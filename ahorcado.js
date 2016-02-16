cc92164f142beda3cb786a15d686b42ee5ea1513
andres <andres.alas@hotmail.com>
2016-02-16 09:24:56 -0600

    Usando prototype

--- ahorcado.js
+++ ahorcado.js
@@ -1,4 +1,21 @@
 function inicio()
 {
-	
+
 }
+
+var Mascota = function(n,f,v)
+{
+	this.nombre = n;
+	this.fuerza = f;
+	this.voz = v;
+}	
+
+Mascota.prototype.hablar = function()
+{
+	alert(this.nombre + " dice: " + this.voz);
+}
+
+var perro = new Mascota("Dagoberto",100,"guua");
+var gato = new Mascota("misifu",25,"miau");
+perro.hablar();
+gato.hablar();
